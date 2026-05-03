5d7dfd9ab7eca530e76b94649d134c56785053d2
Chris Crocker-White <chriscw@balena.io>
2026-05-03 16:42:31 +0100

    Fixed Spotify widget fixes #2 and randomised homepage image fixes #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,40 @@
-function getSpotifyInfo() {
-  
-  $.getJSON('https://ccwspotify.crocker-white.com/spotify.php', null, function(res) {
-    $('#spotify-info-container').animate({ 'opacity' : 1 }, 400);
+function getLastFmInfo() {
+  fetch('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=chrisys&api_key=6fcc9e217642b34260dfea3dd4e9ed5d&format=json&limit=1')
+    .then(function(response) { return response.json(); })
+    .then(function(res) {
+      const track = res.recenttracks.track[0];
+      if (!track) return;
 
-    if(res.nowplaying==true)
-    {
-      $('#spotify-eq').animate({ 'opacity' : 1 }, 400);
-    } else {
-      $('#spotify-eq').animate({ 'opacity' : 0 }, 400);
-    }
+      const nowPlaying = track['@attr'] && track['@attr'].nowplaying === 'true';
+      const trackName = track.name;
+      const artistName = track.artist['#text'];
 
-    if($('#spotify-trackname').html() != res.spotifytrackname)
-    {
-      $('#spotify-trackname').animate({
-        'opacity' : 0
-      }, 400, function() {
-        $(this).html(res.spotifytrackname).animate({ 'opacity' : 1 }, 400);
-      });
-    }
+      if (!nowPlaying) {
+        const playedAt = parseInt(track.date.uts, 10) * 1000;
+        const ageHours = (Date.now() - playedAt) / (1000 * 60 * 60);
+        if (ageHours > 24) return;
+      }
 
-    if($('#spotify-artistname').html() != $('<div/>').text(res.spotifyartistname).html())
-    {
-      $('#spotify-artistname').animate({
-        'opacity' : 0
-      }, 400, function() {
-        $(this).html(res.spotifyartistname).animate({ 'opacity' : 1 }, 400);
-      });
-    }
+      $('#listening-label').text(nowPlaying ? 'Now playing' : 'Last played');
+      $('#spotify-eq').css('opacity', nowPlaying ? 1 : 0);
+      $('#spotify-info-container').css('opacity', 1);
 
-    if($('#spotify-devicename').html() != $('<div/>').text(res.spotifydevicename).html())
-    {
-      $('#spotify-devicename').animate({
-        'opacity' : 0
-      }, 400, function() {
-        $(this).html(res.spotifydevicename).animate({ 'opacity' : 1 }, 400);
-      });
-    }
-  });
+      if ($('#spotify-trackname').html() != trackName) {
+        $('#spotify-trackname').animate({ 'opacity': 0 }, 400, function() {
+          $(this).html(trackName).animate({ 'opacity': 1 }, 400);
+        });
+      }
 
+      if ($('#spotify-artistname').html() != $('<div/>').text(artistName).html()) {
+        $('#spotify-artistname').animate({ 'opacity': 0 }, 400, function() {
+          $(this).html(artistName).animate({ 'opacity': 1 }, 400);
+        });
+      }
+    })
+    .catch(function(err) { console.error('Last.fm fetch failed:', err); });
 }
 
-if($("#spotify-info-container").length) {
-    // if the spotify info container exists, set an interval to update it occasionally
-    getSpotifyInfo();
-    setInterval(getSpotifyInfo, 5000);
-  }
-  
+if ($('#spotify-info-container').length) {
+  getLastFmInfo();
+  setInterval(getLastFmInfo, 60000);
+}
